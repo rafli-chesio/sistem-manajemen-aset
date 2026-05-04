3960883a8e5dd94c4b4f25917a8ef46cfcd08cb8
rafli <rafli.chesio@gmail.com>
2026-05-05 02:24:53 +0700

    asas

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -16,9 +16,7 @@ use Inertia\Response;
 
 class RegisteredUserController extends Controller
 {
-    /**
-     * Display the registration view.
-     */
+
     public function create(): Response
     {
         return Inertia::render('Auth/Register');
